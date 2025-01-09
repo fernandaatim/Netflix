8e17dca5ab929c563dddb3168f731d8447bdde2e
Fernanda <143547756+fernandaatim@users.noreply.github.com>
2025-01-08 21:44:08 -0300

    :tada: Initial commit:  main layout configuration, ActionBar setup, and initial design adjustments.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,16 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    kotlin("kapt")
 }
 
 android {
     namespace = "com.cursoandroid.netflix"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.cursoandroid.netflix"
+        applicationId = "com.cursoandroid.meuprimeiroprojeto"
         minSdk = 33
-        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -33,15 +33,24 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+    }
+
+    buildToolsVersion = "35.0.0"
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.lifecycle.viewmodel)
+    implementation(libs.androidx.lifecycle.livedata)
+    implementation(libs.androidx.lifecycle.compiler)
+    kapt(libs.androidx.lifecycle.compiler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
